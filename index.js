30f292a4a746cae248713fc64d7e2620d405b30a
Jeffrey Vandenborne <vandenbornejeffrey@gmail.com>
2015-01-27 21:31:19 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+"use strict";
+
+module.exports = function (task) {
+  task.setRequirements(['gulp-autoprefixer']);
+  task.exec(function () {
+    var laravel = this.getLaravel(),
+        gulp = this,
+        baseDir = laravel.config.cssOutput,
+        cssFiles = baseDir + '/**/*.css';
+
+    if (laravel.config.production) {
+      return gulp.src(cssFiles)
+        .pipe(task.plugins.autoprefixer())
+        .pipe(gulp.dest(baseDir));
+    }
+
+    laravel.notifier.message('Not in production environment, not minimizing css');
+  });
+};
